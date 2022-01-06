3e999afbebcc82bf2795ce7b0250eb13770f6ee3
Wolfgang Maier <w.maier74@gmx.de>
2022-01-06 18:47:37 +0100

    Einbindungen mit Parametrisierung fertig.

--- FlowProtocol/Core/TemplateReader.cs
+++ FlowProtocol/Core/TemplateReader.cs
@@ -6,99 +6,147 @@ namespace FlowProtocol.Core
 
     public class TemplateReader
    {
+       // Liste der einzelnen Template-Ebenen in Abhängigkeit der Einrückung
        Dictionary<int, Template> TemplateList = new Dictionary<int, Template>();
+       // Liste der einzelnen Restriction-Ebenen in Abhängigkeit der Einrückung
        Dictionary<int, Restriction> ResttrictionList = new Dictionary<int, Restriction>();
        
+       // Liest eine Template-Datei ein
+       // filepath = der vollständige Dateipfad
        public Template? ReadTemplate(string filepath)
        {
            if (!File.Exists(filepath)) return null;
            Template main = new Template();
+           Dictionary<string, string> assignments = new Dictionary<string, string>();
            TemplateList[-1] = main;
-           ReadTemplatePart(filepath, ref main, 0);
+           ReadTemplatePart(filepath, ref main, 0, assignments);
            return main;
        }
 
-       private void ReadTemplatePart(string filepath, ref Template main, int masterindent)
-       {
-           
-           using (StreamReader sr = new StreamReader(filepath))
-           {
-               Regex regRestriction = new Regex(@"\?(.*):(.*)");
-               Regex regOption = new Regex("#(.*):(.*)");
-               Regex regTodo = new Regex(">>(.*)");
-               Regex regInsert = new Regex(@"\+\+(.*):(.*)");                        
-               while (sr.Peek() != -1)
-               {
-                   string? line = sr.ReadLine();
-                   if (!string.IsNullOrWhiteSpace(line))
-                   {                       
-                       line = line.Replace("\t", "    ");
-                       int indent = masterindent + line.Length-line.TrimStart().Length;
-                       string codeline = line.Trim();
-                       if (regRestriction.IsMatch(codeline))
-                       {                           
-                           Template? parent = GetMatchingParent(indent, TemplateList);
-                           if (parent != null)
-                           {
-                               var m = regRestriction.Match(codeline);
-                               Restriction r = new Restriction(){Key = m.Groups[1].Value.Trim(), QuestionText = m.Groups[2].Value.Trim()};
-                               parent.Restrictions.Add(r);
-                               ResttrictionList[indent] = r;
-                           }
-                       }
-                       else if (regOption.IsMatch(codeline))
-                       {
-                           Restriction? parent = GetMatchingParent(indent, ResttrictionList);
-                           if (parent != null)
-                           {
-                               var m = regOption.Match(codeline);
-                               Option o = new Option(){Key = m.Groups[1].Value.Trim(), OptionText = m.Groups[2].Value.Trim()};
-                               parent.Options.Add(o);
-                               TemplateList[indent] = o;
-                           }
-                       }
-                       else if (regTodo.IsMatch(codeline))
-                       {
-                           Template? parent = GetMatchingParent(indent, TemplateList);
-                           if (parent != null)
-                           {
-                               var m = regTodo.Match(codeline);
-                               ToDo t = new ToDo(){ToDoText = m.Groups[1].Value.Trim()};
-                               parent.ToDos.Add(t);
-                           }
-                       }
-                       else if (regInsert.IsMatch(codeline))
-                       {
-                           Template? parent = GetMatchingParent(indent, TemplateList);
-                           if (parent != null)
-                           {
-                               var m = regInsert.Match(codeline);
-                               string flowFunctionFilepath = new FileInfo(filepath).DirectoryName + "\\" + m.Groups[1] + ".qff";
-                               if (File.Exists(flowFunctionFilepath))
-                               {
-                                   ReadTemplatePart(flowFunctionFilepath, ref parent, indent);
-                               }
-                           }
-                       }
-                   }
-               }
-           }           
+       // Liest eine Template-Datei oder eine Template-Funktionsdatei aus
+       // main = die aktuelle Template-Ebene auf der eingelesen wird
+       // masterindent = die Einrücktiefe an der aufrufenden Stelle
+       // filepath = der vollständige Dateipfad
+       private void ReadTemplatePart(string filepath, ref Template main, int masterindent, Dictionary<string, string> assignments)
+       {           
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                Regex regComment = new Regex("//.*");
+                Regex regRestriction = new Regex(@"\?(.*):(.*)");
+                Regex regOption = new Regex("#(.*):(.*)");
+                Regex regTodo = new Regex(">>(.*)");
+                Regex regInsert = new Regex(@"\+\+(.*):(.*)");                        
+                while (sr.Peek() != -1)
+                {
+                    string? line = sr.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {                       
+                        line = line.Replace("\t", "    ");                       
+                        int indent = masterindent + line.Length-line.TrimStart().Length;
+                        line = ReplaceVariables(line, assignments);
+                        string codeline = line.Trim();
+                        if (regComment.IsMatch(codeline))
+                        {
+                            // Nur Kommentar: ignorieren
+                        }
+                        else if (regRestriction.IsMatch(codeline))
+                        {                           
+                            Template? parent = GetMatchingParent(indent, TemplateList);
+                            if (parent != null)
+                            {
+                                var m = regRestriction.Match(codeline);
+                                Restriction r = new Restriction(){Key = m.Groups[1].Value.Trim(), QuestionText = m.Groups[2].Value.Trim()};
+                                parent.Restrictions.Add(r);
+                                ResttrictionList[indent] = r;
+                            }
+                        }
+                        else if (regOption.IsMatch(codeline))
+                        {
+                            Restriction? parent = GetMatchingParent(indent, ResttrictionList);
+                            if (parent != null)
+                            {
+                                var m = regOption.Match(codeline);
+                                Option o = new Option(){Key = m.Groups[1].Value.Trim(), OptionText = m.Groups[2].Value.Trim()};
+                                parent.Options.Add(o);
+                                TemplateList[indent] = o;
+                            }
+                        }
+                        else if (regTodo.IsMatch(codeline))
+                        {
+                            Template? parent = GetMatchingParent(indent, TemplateList);
+                            if (parent != null)
+                            {
+                                var m = regTodo.Match(codeline);
+                                ToDo t = new ToDo(){ToDoText = m.Groups[1].Value.Trim()};
+                                parent.ToDos.Add(t);
+                            }
+                        }
+                        else if (regInsert.IsMatch(codeline))
+                        {
+                            Template? parent = GetMatchingParent(indent, TemplateList);
+                            if (parent != null)
+                            {
+                                var m = regInsert.Match(codeline);
+                                string flowFunctionFilepath 
+                                    = new FileInfo(filepath).DirectoryName + "\\" + m.Groups[1].Value.Trim().Replace(".qff", string.Empty) + ".qff";
+                                if (File.Exists(flowFunctionFilepath))
+                                {
+                                    Dictionary<string, string> subassignments = ReadAssignments(m.Groups[2].Value);
+                                    ReadTemplatePart(flowFunctionFilepath, ref parent, indent, subassignments);
+                                }
+                            }
+                        }
+                    }
+                }    
+            }           
        }
 
        // Sucht das passende Parent-Objekt anhand der Einrückung aus der Liste heraus
-       T? GetMatchingParent<T>(int indent, Dictionary<int, T> list) where T : class
+       private T? GetMatchingParent<T>(int indent, Dictionary<int, T> list) where T : class
        {
-           int bestMatch = -1;
-           T? ret = null;
-           foreach(var idx in list)
-           {
-               if (idx.Key < indent && idx.Key >= bestMatch)
-               {
-                   ret = idx.Value;
-                   bestMatch = idx.Key;
-               }
+            int bestMatch = -1;
+            T? ret = null;
+            foreach(var idx in list)
+            {
+                if (idx.Key < indent && idx.Key >= bestMatch)
+                {
+                    ret = idx.Value;
+                    bestMatch = idx.Key;
+                }
+            }
+            return ret;
+        }
+
+        // Liest aus einem Ausdruck "$V1=X; $V2=Y" die Variablenzuweisungen aus und gibt diese zurück.
+        private Dictionary<string, string> ReadAssignments(string? varExpression)
+        {
+            Dictionary<string, string> assignments = new Dictionary<string, string>();
+            if (!string.IsNullOrWhiteSpace(varExpression))
+            {
+                Regex regAssignement = new Regex(@"\$([A-Za-z0-9]*)=(.*)");
+                foreach(var idx in varExpression.Split(";"))
+                {
+                    string assignment = idx.Trim();
+                    if (regAssignement.IsMatch(assignment))
+                    {
+                        var m = regAssignement.Match(assignment);
+                        assignments[m.Groups[1].Value.Trim()] = m.Groups[2].Value.Trim();
+                    }
+                }
            }
-           return ret;
+           return assignments;
        }
-   }
+
+       // Ersetzt die Variablen durch die Werte.
+       private string ReplaceVariables(string codeline, Dictionary<string, string> assignments)
+        {
+            if (string.IsNullOrWhiteSpace(codeline)) return codeline;
+            string ret = codeline;
+            foreach(var idx in assignments)
+            {
+                ret = ret.Replace("$" + idx.Key, idx.Value);
+            }
+            return ret;
+        }
+    }
 }
--- README.md
+++ README.md
@@ -7,18 +7,32 @@ Diese Vorgehensweise ist vielseitig einsetzbar und daher auch für verschiedene 
 
 ## Vorlagen
 Die Vorlagen sind die Kernelemente von FlowProtocol. Sie beinhalten die Fragen samt Antwortmöglichkeiten und die davon abhängigen Prüfpunkte. Jede Vorlage sollte sich auf ein spezielles Thema oder eine bestimmte Art von Aufgabenstellung beziehen, um möglichst direkt mit möglichst spezifischen Fragen starten zu können. Ein solches dem Gebiet kann zum Beispiel eine bestimmte Art von Entwicklung sein, die nach einem vorgegebenen Muster abläuft und darin überschaubare Varianten aufweisen kann. Diese lassen sich dann mithilfe von passenden Fragen eingrenzen. In gleicher Weise kann man auch fachliche Themen in Form einer Vorlage bereitstellen, indem man versucht, die komplette fachliche Spezifikation zu erfassen, und deren potentielle Beeinflussung durch eine Maßnahme durch geeignete Fragen festzustellen. Vorlagen können wiederum in Vorlagengruppen gruppiert werden, etwa wie in den gerade genannten Beispielen in „Musterentwicklungen“ und „Fachliche Themen“, eventuell zusätzlich unterteilt durch Produkt-, und Komponentennamen. Die Unterteilung erfolgt in gleicher Weise wie bei den Anwendergruppen durch entsprechend benannte Unterverzeichnisse unterhalb der Anwendergruppen-Verzeichnisse. Eine mehrstufige Hierarchie ist möglich, wird jedoch in der Darstellung ignoriert.
-Aufbau einer Vorlage
 
-Vorlagen sind einfache Textdateien mit der Dateiendung „*.qfp“ (Quick Flow Protocol). Die Syntax besteht aus ganz wenigen Steuerzeichen und wurde speziell darauf ausgelegt, dass Vorlagen sehr einfach mit einem beliebigen Editor erstellt und bearbeitet werden können. Der Inhalt einer Vorlage besteht im Wesentlichen aus den drei Komponenten Frage (?), Antwort (#) und Prüfpunkt (>>), die in beliebiger Tiefe verschachtelt werden können. Fragen und Antworten haben jeweils eine möglichst kurze Kennung, mit denen der Verlauf der Durchführung als URL-Parameter abgebildet wird. Zeilenumbrüche und Einrückungen sind essenziell zur Abbildung der Struktur, wobei die Anzahl der Zeichen relevant ist. Das nachfolgende Beispiel zeigt, wie die Syntax aufgebaut ist:
+
+## Aufbau einer Vorlage
+Vorlagen sind einfache Textdateien mit der Dateiendung „*.qfp“ (Quick Flow Protocol). Die Syntax besteht aus ganz wenigen Steuerzeichen und wurde speziell darauf ausgelegt, dass Vorlagen sehr einfach mit einem beliebigen Editor erstellt und bearbeitet werden können. Der Inhalt einer Vorlage besteht im Wesentlichen aus den Komponenten Kommentar (//), Frage (?), Antwort (#), Prüfpunkt (>>) und Einbindungen (++), die in beliebiger Tiefe verschachtelt werden können. Fragen und Antworten haben jeweils eine möglichst kurze Kennung, mit denen der Verlauf der Durchführung als URL-Parameter abgebildet wird. Zeilenumbrüche und Einrückungen sind essenziell zur Abbildung der Struktur, wobei die Anzahl der Zeichen relevant ist. Das nachfolgende Beispiel zeigt, wie die Syntax aufgebaut ist:
 ```
+// Diese Vorlage umfasst Prüffragen für die Erstellung von Listenbereichen.
 ?F1: Welche Art von Liste wurde erstellt?
     #E: Editierbare Liste
         >> Elemente in der Liste können bearbeitet werden.
         ?E1: Können im Umfeld der Liste neue Elemente erstellt werden?
             #J: Ja
                 >> Neu erstellte Elemente werden direkt in der Liste angezeigt.
+                ++ListeErstellung: $Bereich=Test; $Postfix=1
             #N: Nein
     #A: Auswahlliste
         >> Elemente in der Liste können nicht bearbeitet werden.
         >> Die Auswahl einzelner Elemente ist möglich und wird korrekt verarbeitet. 
 ```
+
+## Einbindungen
+Manche Abschnitte aus Vorlagen sind eventuell so allgemein, dass man sie in gleichger Form in mehreren Vorlagen verwenden möchte. In diesem Fall kann man diese Abschnitte als Funktionsdatei mit der Dateiendung „*.qff“ (Quick Flow Protocol) in eine eigene Datei auslagern und über eine Einbindung (++Dateiname) an einer bestimmten Stelle in einer Vorlage aufrufen. Die Datei muss dabei im gleichen Verzeichnis liegen. Bei einem solchen Aufruf lassen sich auch Variablen mit Werte belegen, z.B. $Bereich=Test; $Postfix=1, so dass beim Einlsen der Datei Ersetzungen möglich sind, z.B.
+```
+?T$Postfix: Besitzt der Bereich $Bereich einen Berichtskontext?
+```
+zu
+```
+?T1: Besitzt der Bereich Test einen Berichtskontext?
+```
+wodurch auch mehfache Aufrufe innerhalb derselben Vorlage möglich sind.
