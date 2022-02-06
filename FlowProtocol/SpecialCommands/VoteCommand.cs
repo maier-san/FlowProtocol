593180f9fa929f0bc47dac353b64fd5301922d34
Wolfgang Maier <w.maier74@gmx.de>
2022-02-06 15:40:03 +0100

    Groupsize-Parameter

--- FlowProtocol/SpecialCommands/VoteCommand.cs
+++ FlowProtocol/SpecialCommands/VoteCommand.cs
@@ -9,7 +9,8 @@ namespace FlowProtocol.SpecialCommands
         {
             List<ResultItem> ril = new List<ResultItem>();
             Restriction? res = GetRestriction(cmd, template, addError);
-            SetCrossTemplates(template, res);
+            int groupsize = GetParameter(cmd, "GroupSize", 3, addError);
+            SetCrossTemplates(template, res, groupsize);
             
             return ril;
        }
@@ -30,28 +31,48 @@ namespace FlowProtocol.SpecialCommands
             return res;
        }
 
-       private void SetCrossTemplates(Template template, Restriction? res)
+       private int GetParameter(Command cmd, string name, int defaultvalue, Action<ReadErrorItem> addError)
+       {
+            int ret = defaultvalue;
+            Regex reg = new Regex(name + "=([0-9]*)");
+            string arguments = cmd.Arguments;
+            Match match = reg.Match(arguments);
+            if (match.Success)
+            {
+                string value = match.Groups[1].Value.Trim();
+                bool ok = int.TryParse(value, out ret);
+                if (!ok)
+                {
+                    ret = defaultvalue;
+                    CreateError(addError, "V03", $"Vote-Befehl ohne gültiges {name}-Argument", cmd);
+                }
+            }
+            return ret;
+       }
+
+       private void SetCrossTemplates(Template template, Restriction? res, int groupsize)
        {
            if (res == null) return;
            Template t = template;
            Template? orgfollow = template.FollowTemplate;
-           bool createfollowtemplate = false;
+           int groupcount = 0;
            foreach (var i1 in res.Options)
            {
                foreach(var i2 in res.Options)
                {
                    if (string.Compare(i1.Key, i2.Key) < 0)
                    {
-                       Restriction ncres = new Restriction(){Key = i1.Key + "_" + i2.Key, QuestionText = res.QuestionText};
+                       Restriction ncres = new Restriction(){Key = i1.Key + i2.Key, QuestionText = res.QuestionText};
                        ncres.Options.Add(new Option(){Key = i1.Key, OptionText = i1.OptionText});
-                       ncres.Options.Add(new Option(){Key = i2.Key, OptionText = i2.OptionText});
-                       if (createfollowtemplate)
+                       ncres.Options.Add(new Option(){Key = i2.Key, OptionText = i2.OptionText});                       
+                       if (groupcount >= groupsize)
                        {
                            t.FollowTemplate = new Template();
                            t = t.FollowTemplate;
+                           groupcount = 0;
                        }
+                       groupcount++;
                        t.Restrictions.Add(ncres);
-                       createfollowtemplate = true;
                    }
                }
            }
