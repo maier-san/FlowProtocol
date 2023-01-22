58b3a67f18a61bf5c8a046519da02c3c6a2481a6
Wolfgang Maier <w.maier74@gmx.de>
2023-01-22 17:18:59 +0100

    Basisklasse aus Resultitem abgeleitet

---
+++ FlowProtocol/Core/FlowItem.cs
@@ -0,0 +1,12 @@
+namespace FlowProtocol.Core
+{
+    public class FlowItem
+    {
+        public string SortPath { get; set; }
+
+        public FlowItem()
+        {
+            SortPath = string.Empty;
+        }
+    }       
+}
