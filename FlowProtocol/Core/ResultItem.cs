f532c898d125d73ab4b67486ffcb4b268225603f
Wolfgang Maier <w.maier74@gmx.de>
2023-02-05 17:57:15 +0100

    Empty-Codeline-Problem gelöst

--- FlowProtocol/Core/ResultItem.cs
+++ FlowProtocol/Core/ResultItem.cs
@@ -6,13 +6,15 @@
         public string ResultItemGroup { get; set; }
         public List<string> SubItems { get; set; }
         public string CodeBlock { get; set; }
-        
+        private bool CodeExists { get; set; }
+
         public ResultItem() : base()
         {
             ResultItemText = string.Empty;
             ResultItemGroup = string.Empty;
             SubItems = new List<string>();
-            CodeBlock = string.Empty;            
+            CodeBlock = string.Empty;
+            CodeExists = false;
         }
 
         // Wendet eine Text-Operation auf die Text-Bestandteile Ergebniseintrags an.
@@ -28,8 +30,9 @@
         // Fügt eine Codezeile hinzu
         public void AddCodeLine(string codeline)
         {
-            // Hier nicht mit Environment.newline arbeiten, da sonst beim ausschneiden und kopieren des Codes unerwünschte Leerzeilen eingefügt werden.
-            if (CodeBlock == string.Empty) CodeBlock = codeline; else CodeBlock += "\n" + codeline;
+            // Hier nicht mit Environment.newline arbeiten, da sonst beim ausschneiden und kopieren des Codes unerwünschte Leerzeilen eingefügt werden.            
+            if (!CodeExists) CodeBlock = codeline; else CodeBlock += "\n" + codeline;
+            CodeExists = true;
         }
     }
 }
